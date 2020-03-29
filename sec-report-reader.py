bef8a59e0cd614f20e73f5cb9888ae9b6520d5eb
zoltan-szrenko <zoltan.szrenko@gmail.com>
2020-03-29 09:16:46 +0200

    command line arguments handling and ip addr checking

--- sec-report-reader.py
+++ sec-report-reader.py
@@ -2,46 +2,70 @@
 
 from dns import reversename, resolver
 import sys
+import argparse
 import csv
 import socket
 import xlrd
 from xlutils.copy import copy
-#import xlwt
 
-print("start")
+fnamei = "report2.xlsx"
+fnameo = "report3.xlsx"
+rnd_nw = "10.12." # default pattern
+firstrow = 1 # second row
+dnscolumn = 1 # second column
 
-fname = "report2.xlsx"
-sheetname = "in"
-rnd_nw = "10.12."
-firstrow = 1
-dnscolumn = 1
+parser = argparse.ArgumentParser(description='This script read the first column of the given excel sheet and write back the reverse DNS to the second column')
+parser.add_argument('i', metavar='inputfile', action="store", help="input excel file name")
+parser.add_argument('-o', metavar='', action="store", help="output excel file name")
+parser.add_argument('--nw', metavar='', action="store", help="network pattern to filter IP")
+parser.add_argument('--first_row', metavar='', action="store", help="first row where we start to process the IPs", type=int)
+parser.add_argument('--dns_column', metavar='', action="store", help="dns column where we put the DNS names", type=int)
+
+if parser.parse_args().i:
+	fnamei = parser.parse_args().i
+if parser.parse_args().o:
+	fnameo = parser.parse_args().o
+else:
+	fnameo = fnamei
+if parser.parse_args().nw:
+	rnd_nw = parser.parse_args().nw
+if parser.parse_args().first_row:
+	firstrow = parser.parse_args().first_row
+if parser.parse_args().dns_column:
+	dnscolumn = parser.parse_args().dns_column
+
 
 def reversedns(cella):
 	revdns = reversename.from_address(cella)
 	try:
-		print('reversedns: '+dname)
 		return str(resolver.query(revdns,"PTR")[0])
 	except resolver.NXDOMAIN as e:
 		return "NXDOMAIN"
-		#print('NXDOMAIN')
 
+def validateip(ipaddr):
+	try:
+		socket.inet_aton(ipaddr)
+	except socket.error:
+		return False
+	return True
 
-xbook1 = xlrd.open_workbook(fname)
+xbook1 = xlrd.open_workbook(fnamei)
 xsheet1 = xbook1.sheet_by_index(0)
 nrows = xsheet1.nrows
 
 xbook2 = copy(xbook1)
 xsheet2 = xbook2.get_sheet(0)
 
-dname = "dnstest"
+dname = "reversednsfailed"
 #num_cols = xsheet.ncols
 for row_idx in range(firstrow, nrows):
+	ipvalid = True
 	celly = xsheet1.cell_value(row_idx, 0)
-	if len(celly) >= 6:
+	
+	if validateip(celly):
 		if celly[:len(rnd_nw)] == rnd_nw:
 			dname = reversedns(celly)
-			print('celly: '+celly+' dname: '+dname)
+			#print(celly+';'+dname)
 			xsheet2.write(row_idx, dnscolumn, dname)
 
-xbook2.save(fname)
-print("fin")
+xbook2.save(fnameo)
